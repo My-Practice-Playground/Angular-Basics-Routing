8e1bc2de33c9327bf1d63a5e795e63e12d30ae56
Shan <perera.alc2000@gmail.com>
2023-08-21 13:35:41 +0530

    routes added in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {HomeComponent} from "./home/app.home.component";
+import {CustomerComponent} from "./customer/customer.component";
+import {OrderComponent} from "./order/order.component";
 
-const routes: Routes=[];
+const routes: Routes=[
+  {path: 'home', component:HomeComponent},
+  {path: 'customer', component:CustomerComponent},
+  {path: 'order',component:OrderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
